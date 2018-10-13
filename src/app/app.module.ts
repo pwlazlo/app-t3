6a4ecf90766a180a87540ac8ab2a37930109750f
pw <pwlazlo@onet.pl>
2018-10-13 22:33:31 +0200

    a1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCardModule, MatMenuModule, MatToolbarModule, MatIconModule } from '@angular/material';
+/* import { FlexLayoutModule } from '@angular/flex-layout'; */
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +11,13 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule /*, FlexLayoutModule */
   ],
   providers: [],
   bootstrap: [AppComponent]
